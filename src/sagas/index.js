0abb65272f8a411d11666d620ce91f523c833732
Anurag Hakeem <anuhakee@publicisgroupe.net>
2019-07-23 18:25:38 +0530

    Saga basics implemented

---
+++ src/sagas/index.js
@@ -0,0 +1,12 @@
+import { takeEvery, put } from 'redux-saga/effects'
+
+function* workerSaga() {
+    console.log("Hey from Worker")
+    yield put({ type: 'ACTION_FROM_WORKER' })
+}
+
+function* rootSaga() {
+    yield takeEvery('HELLO', workerSaga)
+}
+
+export default rootSaga;
